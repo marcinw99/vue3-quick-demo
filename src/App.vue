baec9a860432453cf92a8bf056cea94eafe76d85
Marcin Wąsik <marcinmw99@gmail.com>
2024-05-04 12:07:45 +0200

    feat: use Composition API and move data handling to App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,24 @@
   <div class="container">
     <Balance />
     <IncomeExpenses />
-    <TransactionList />
+    <TransactionList :transactions="transactions" />
     <AddTransaction />
   </div>
 </template>
 
-<script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
 import Header from './components/Header.vue'
 import Balance from './components/Balance.vue'
 import IncomeExpenses from './components/IncomeExpenses.vue'
 import TransactionList from '@/components/TransactionList.vue'
 import AddTransaction from '@/components/AddTransaction.vue'
 
-export default {
-  components: { Header, Balance, IncomeExpenses, TransactionList, AddTransaction }
-}
+const transactions = ref([
+  { id: 1, text: 'Flower', amount: -20 },
+  { id: 2, text: 'Salary', amount: 300 },
+  { id: 3, text: 'Book', amount: -10 },
+  { id: 4, text: 'Camera', amount: 150 }
+])
 </script>
